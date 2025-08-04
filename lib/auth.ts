568688ff0d9a9eb6d4a7fb4ce86738d2a01c9515
yaredow <yaredyilma11@gmail.com>
2025-08-04 12:43:19 +0300

    configure better-auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,16 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db";
+import { fayda } from "fayda";
 
 export const auth = betterAuth({
-	database: {},
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	plugins: [
+		await fayda({
+			clientId: process.env.CLIENT_ID as string,
+			privateKey: process.env.PRIVATE_KEY as string,
+		}),
+	],
 });
